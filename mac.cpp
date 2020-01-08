01aff1241fa42de63c4cc9f12e995e3f5fafb3e1
Matthew Martin <martim01@outlook.com>
2020-01-08 08:26:45 +0000

    feat: adding linux bit for generating clock id - note this is some random code and needs sorting

--- mac.cpp
+++ mac.cpp
@@ -8,6 +8,89 @@
 #include <sstream>
 #include <iomanip>
 
+
+#ifdef __GNU__
+unsigned long long int GenerateClockIdentity(const std::string& sIpAddress)
+{
+   	 int fd;
+	
+	struct ifreq ifr;
+	char *iface = "eth0";
+	char *mac;
+	
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy((char *)ifr.ifr_name , (const char *)iface , IFNAMSIZ-1);
+
+	ioctl(fd, SIOCGIFHWADDR, &ifr);
+
+	close(fd);
+	
+	mac = (char *)ifr.ifr_hwaddr.sa_data;
+	
+	//display mac address
+	sprintf((char *)uc_Mac,(const char *)"%.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	
+	
+	struct ifaddrs *ifaddr, *ifa;
+           int family, s, n;
+           char host[NI_MAXHOST];
+
+           if (getifaddrs(&ifaddr) == -1) {
+               perror("getifaddrs");
+               exit(EXIT_FAILURE);
+           }
+
+           /* Walk through linked list, maintaining head pointer so we
+              can free list later */
+
+           for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+               if (ifa->ifa_addr == NULL)
+                   continue;
+
+               family = ifa->ifa_addr->sa_family;
+
+               /* Display interface name and family (including symbolic
+                  form of the latter for the common families) */
+
+               printf("%-8s %s (%d)\n",
+                      ifa->ifa_name,
+                      (family == AF_PACKET) ? "AF_PACKET" :
+                      (family == AF_INET) ? "AF_INET" :
+                      (family == AF_INET6) ? "AF_INET6" : "???",
+                      family);
+
+               /* For an AF_INET* interface address, display the address */
+
+               if (family == AF_INET || family == AF_INET6) {
+                   s = getnameinfo(ifa->ifa_addr,
+                           (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                                                 sizeof(struct sockaddr_in6),
+                           host, NI_MAXHOST,
+                           NULL, 0, NI_NUMERICHOST);
+                   if (s != 0) {
+                       printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                       exit(EXIT_FAILURE);
+                   }
+
+                   printf("\t\taddress: <%s>\n", host);
+
+               } else if (family == AF_PACKET && ifa->ifa_data != NULL) {
+                   struct rtnl_link_stats *stats = ifa->ifa_data;
+
+                   printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
+                          "\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
+                          stats->tx_packets, stats->rx_packets,
+                          stats->tx_bytes, stats->rx_bytes);
+               }
+           }
+
+           freeifaddrs(ifaddr);
+           exit(EXIT_SUCCESS);
+	
+}
+#else
 unsigned long long int GenerateClockIdentity(const std::string& sIpAddress)
 {
     ULONG outBufLen = sizeof(IP_ADAPTER_ADDRESSES);
@@ -56,3 +139,4 @@ unsigned long long int GenerateClockIdentity(const std::string& sIpAddress)
     }
     return 0;
 }
+#endif
